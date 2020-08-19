03f1a1b32f84d4ff3d0e0e86e1722223cd721d05
kifayat26 <kifayat26@gmail.com>
2020-08-19 13:17:54 +0600

    homepage divided into components and implementation of the components

---
+++ src1/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import HomePage from './pages/homepage/homepage.component';
+
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='App'>
+        <HomePage />
+      </div>
+    );
+  }
+}
+
+export default App;
